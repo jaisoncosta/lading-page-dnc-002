1275df9fa726b0477ee9d31a42d12cb7ee5dffdb
Jaison Clementino Costa <jaison.clementino@afya.com.br>
2024-12-19 18:59:03 -0300

    Versao Inicial

---
+++ index.js
@@ -0,0 +1,19 @@
+var SetaEsquerda = window.document.getElementById("seta-esquerda");
+var Leonardo = window.document.getElementById("leonardo");
+var Samantha = window.document.getElementById("samantha");
+var Bruna = window.document.getElementById("bruna");
+var SetaDireita = window.document.getElementById("seta-direita");
+
+function RolarParaDireita() {
+    Leonardo.style = "display:none"
+    Bruna.style = "display:flex"
+    SetaDireita.style = "display:none"
+    SetaEsquerda.style = "display:flex"
+}
+
+function RolarParaEsquerda() {
+    Leonardo.style = "display:flex"
+    Bruna.style = "display:none"
+    SetaDireita.style = "display:flex"
+    SetaEsquerda.style = "display:none"
+}
